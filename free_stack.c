1d2a2c0dd27414d405b5413b6e7b2b5682f0e9ee
fionamokua <mokuafiona@gmail.com>
2023-10-19 04:49:55 -0700

    add

---
+++ free_stack.c
@@ -0,0 +1,17 @@
+#include "monty.h"
+/**
+* free_stack - frees a doubly linked list
+* @head: head of the stack
+*/
+void free_stack(stack_t *head)
+{
+	stack_t *result;
+
+	 result = head;
+	while (head)
+	{
+		result = head->next;
+		free(head);
+		head = result;
+	}
+
