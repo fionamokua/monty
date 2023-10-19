76784c528393442677862b7a28daed46ba22da70
fionamokua <mokuafiona@gmail.com>
2023-10-19 07:19:06 -0700

    betty

--- pall.c
+++ pall.c
@@ -2,13 +2,13 @@
 /**
  * f_pall - prints the stack
  * @head: stack head
- * @counter: no used
+ * @lineNum: no used
  * Return: no return
 */
-void f_pall(stack_t **head, unsigned int lineNumber)
+void f_pall(stack_t **head, unsigned int lineNum)
 {
 	stack_t *h;
-	(void)lineNumber;
+	(void)lineNum;
 
 	h = *head;
 	if (h == NULL)
