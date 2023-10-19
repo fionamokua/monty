20fd290436e2265f01e78e321d30dede480f1ad0
fionamokua <mokuafiona@gmail.com>
2023-10-19 04:41:10 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top
+ * @head: stack head
+ * @lineNumber: line_number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int lineNumber)
+{
+	(void)head;
+	(void)lineNumber;
+	bus.lifi = 0;
+}
